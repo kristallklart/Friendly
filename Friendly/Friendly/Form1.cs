98d357ac6c2fcbdcbbc9eb28c988e5f23809a2fd
bjarnmark <anna.bjarnmar@gmail.com>
2017-02-15 10:16:12 +0100

    Väldigt snyggt gooey

--- Friendly/Friendly/Form1.cs
+++ Friendly/Friendly/Form1.cs
@@ -10,14 +10,78 @@ using System.Windows.Forms;
 
 namespace Friendly
 {
-    public partial class Form1 : Form
+    public partial class FormContainer : Form
     {
-        public Form1()
+        public FormContainer()
         {
             InitializeComponent();
+            for (int i = 1940; i <= 1999; i++)
+            {
+                comboBox_Year.Items.Add(i);
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabSearchMatch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabMyProfile_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void txtBox_ProfessionalTitle_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void picBox_ProfilePic_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_From_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox_Hometown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox_Month_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox_Money_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox_Time_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_To_Click(object sender, EventArgs e)
         {
 
         }
