ebf316bf6a5c35586b2f2d18a56bee8a9e8247db
paulssonkalle <paulsson.kalle@gmail.com>
2017-02-15 15:43:28 +0100

    fixade lite med inloggningen

--- Friendly/Friendly/View/MainForm.cs
+++ Friendly/Friendly/View/MainForm.cs
@@ -23,67 +23,9 @@ namespace Friendly.View
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-
+            this.Hide();
+            LoginForm loginForm = new LoginForm();
+            loginForm.ShowDialog();
         }
-
-        //private void tabSearchMatch_Click(object sender, EventArgs e)
-        //{
-
-        //}
-
-        //private void tabMyProfile_Click(object sender, EventArgs e)
-        //{
-            
-        //}
-
-        //private void panel1_Paint(object sender, PaintEventArgs e)
-        //{
-
-        //}
-
-        //private void txtBox_ProfessionalTitle_TextChanged(object sender, EventArgs e)
-        //{
-
-        //}
-
-        //private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
-        //{
-
-        //}
-
-        //private void picBox_ProfilePic_Click(object sender, EventArgs e)
-        //{
-
-        //}
-
-        //private void label_From_Click(object sender, EventArgs e)
-        //{
-
-        //}
-
-        //private void comboBox_Hometown_SelectedIndexChanged(object sender, EventArgs e)
-        //{
-
-        //}
-
-        //private void comboBox_Month_SelectedIndexChanged(object sender, EventArgs e)
-        //{
-
-        //}
-
-        //private void checkBox_Money_CheckedChanged(object sender, EventArgs e)
-        //{
-
-        //}
-
-        //private void checkBox_Time_CheckedChanged(object sender, EventArgs e)
-        //{
-
-        //}
-
-        //private void label_To_Click(object sender, EventArgs e)
-        //{
-
-        //}
     }
 }
