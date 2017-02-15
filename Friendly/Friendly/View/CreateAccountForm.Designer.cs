95a99cbea2a64333c8f3530e2361582130f60986
paulssonkalle <paulsson.kalle@gmail.com>
2017-02-15 17:10:11 +0100

    räven cachade över isen

---
+++ Friendly/Friendly/View/CreateAccountForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Friendly.View
+{
+    partial class CreateAccountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelRetypedPassword = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxRetypedPassword = new System.Windows.Forms.TextBox();
+            this.buttonCreateAccount = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(117, 109);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(58, 13);
+            this.labelUsername.TabIndex = 0;
+            this.labelUsername.Text = "Username:";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(117, 135);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(56, 13);
+            this.labelPassword.TabIndex = 1;
+            this.labelPassword.Text = "Password:";
+            // 
+            // labelRetypedPassword
+            // 
+            this.labelRetypedPassword.AutoSize = true;
+            this.labelRetypedPassword.Location = new System.Drawing.Point(83, 161);
+            this.labelRetypedPassword.Name = "labelRetypedPassword";
+            this.labelRetypedPassword.Size = new System.Drawing.Size(92, 13);
+            this.labelRetypedPassword.TabIndex = 2;
+            this.labelRetypedPassword.Text = "Retype password:";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(179, 106);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(136, 20);
+            this.textBoxUsername.TabIndex = 3;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(179, 132);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(136, 20);
+            this.textBoxPassword.TabIndex = 4;
+            // 
+            // textBoxRetypedPassword
+            // 
+            this.textBoxRetypedPassword.Location = new System.Drawing.Point(179, 158);
+            this.textBoxRetypedPassword.Name = "textBoxRetypedPassword";
+            this.textBoxRetypedPassword.Size = new System.Drawing.Size(136, 20);
+            this.textBoxRetypedPassword.TabIndex = 5;
+            // 
+            // buttonCreateAccount
+            // 
+            this.buttonCreateAccount.Location = new System.Drawing.Point(217, 184);
+            this.buttonCreateAccount.Name = "buttonCreateAccount";
+            this.buttonCreateAccount.Size = new System.Drawing.Size(98, 23);
+            this.buttonCreateAccount.TabIndex = 6;
+            this.buttonCreateAccount.Text = "Create Account";
+            this.buttonCreateAccount.UseVisualStyleBackColor = true;
+            // 
+            // CreateAccountForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 343);
+            this.Controls.Add(this.buttonCreateAccount);
+            this.Controls.Add(this.textBoxRetypedPassword);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.labelRetypedPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUsername);
+            this.Name = "CreateAccountForm";
+            this.Text = "Create Account";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelRetypedPassword;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxRetypedPassword;
+        private System.Windows.Forms.Button buttonCreateAccount;
+    }
+}
