d79ea4b601cea3619c637b884e8f5b25a38e43da
paulssonkalle <paulsson.kalle@gmail.com>
2017-02-16 17:41:19 +0100

    lade till watersmaterboxen

--- Friendly/Friendly/Utilities/Utils.cs
+++ Friendly/Friendly/Utilities/Utils.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Friendly.Utilities
 {
     public static class Utils
     {
+        public static bool CheckTextBoxes(Form form)
+        {
+
+            return false;
+        }
     }
 }
